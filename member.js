496721421cddf0b4f70e382f12e30215e0bdc0ac
Lenildo Silva Damasceno <lenildo58479636@edum.rn.senac.br>
2025-09-04 02:47:47 +0000

    feat: adiciona member.js

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember () {return{
+    restrict: 'E',
+    template: '<div>Skills Member Component</div>'
+    controller: function($scope) {
+        $scope.message = "Hello from Skills Member";
+    }   
+}
+
